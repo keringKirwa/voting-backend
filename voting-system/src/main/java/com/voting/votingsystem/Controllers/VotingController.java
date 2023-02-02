aa1a8375212c07c523a69534e5aa50f35c2f3d77
arapkering <kkirwa230@gmail.com>
2023-02-02 16:40:56 +0300

    Added the lib folder to the app

--- voting-system/src/main/java/com/voting/votingsystem/Controllers/VotingController.java
+++ voting-system/src/main/java/com/voting/votingsystem/Controllers/VotingController.java
@@ -1,17 +1,44 @@
 package com.voting.votingsystem.Controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.voting.votingsystem.Entities.Body;
+import com.voting.votingsystem.Entities.Item;
+import com.voting.votingsystem.lib.DateFormatter;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class VotingController {
 
     @PostMapping("/mpesa")
     public String createCallback(@RequestBody String jsonString) {
+        DateFormatter dateFormatter=new DateFormatter();
         try{
             ObjectMapper mapper = new ObjectMapper();
             Body paymentDetails = mapper.readValue(jsonString, Body.class);
-            System.out.println(paymentDetails.getStkCallback().getCallback().getCallbackMetadata().getItem().get(0).getValue());
+
+            if(paymentDetails.getStkCallback().getCallback().getCallbackMetadata() != null || paymentDetails.getStkCallback().getCallback().getResultCode()==0){
+
+                System.out.println("JSON REP for a successful payment ::: "+jsonString);
+
+                ArrayList<Item> paymentDetailsArray = (ArrayList<Item>) paymentDetails.getStkCallback().getCallback().getCallbackMetadata().getItem();
+
+
+                int amountPaid= Integer.parseInt(paymentDetailsArray.get(0).getValue());
+                String receiptNo=paymentDetailsArray.get(1).getValue();
+                String unformattedDate= paymentDetailsArray.get(3).getValue();
+                String phoneNumber=paymentDetailsArray.get(4).getValue();
+
+
+
+                String formattedDate= dateFormatter.dateFormatter(unformattedDate);
+
+
+            }else {
+                System.out.println("UNSUCCESSFUL TRANSACTION :: The user cancelled the transaction OR  there is insufficient funds in his or her account !!! ");
+                System.out.println("JSON DATA*(UNSUCCESSFUL) :: "+jsonString);
+            }
+
         }catch(Exception ref){
             ref.printStackTrace();
 
---
+++ voting-system/src/main/java/com/voting/votingsystem/lib/DateFormatter.java
@@ -0,0 +1,22 @@
+package com.voting.votingsystem.lib;
+
+import org.springframework.context.annotation.Bean;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatter {
+
+    public  String dateFormatter(String dateToFormat) throws ParseException {
+        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("MMMM,dd,yyyy HH:mm ");
+
+
+        Date date = inputFormat.parse(dateToFormat);
+        return outputFormat.format(date);
+
+
+    }
+
+}
